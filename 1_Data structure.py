3995122a8103f9b7677c05de8892e0d56ac9ddff
ximitiejiang <ximitiejiang@163.com>
2018-08-25 18:41:24 +0800

    update list & dict skill
    update list & dict skill

--- 1_Data structure.py
+++ 1_Data structure.py
@@ -5,7 +5,7 @@ Created on Fri Aug 24 20:23:37 2018
 
 @author: suliang
 """
-# below are python skills about the data structures and algorithms
+# List, Dictionary
 
 '''
 Q: 
@@ -17,12 +17,20 @@ Q:
 '''
 Q: 有一个包含N个不同类型元素的序列(list/dic/str)，如何把它分解为N个单独的变量？
 '''
-p = (4,5)
-x,y = p     # 该方法可以同时分解成多个变量
+p = [12,5]
+q = 'hello'
+x,y = p           # 用多变量方式可以分解list成多个变量
+a,b,c,d,e = q    # 用多变量方式可以分解string成多个变量
 
 data = ['ACME', 50, 91.1, (2012, 12, 2)]
-name, shares, price, (year, mon, day) = data  # 该方法可以嵌套格式分解出多个变量
-name, _, price, _ = data   # 该下划线方法可以丢弃掉某些不需要的数值
+name, shares, price, (year, mon, day) = data  # 用多变量嵌套格式可以分解出多个变量
+name, _, price, _ = data   # 用下划线方法可以代表某个待丢弃掉的变量
+
+record = {'Dave','dave@example.com','773-555-1212','847-555-1212'}
+name, email, *phone = record  # 用*name变量的形式可以表示开头/中间/末尾多个数据集
+
+record = ['ACME', 50, 91.1, (2012, 12, 2)]
+name, *_, (year, *_) = record  # 用*_代表多个待丢弃的变量
 
 
 '''
@@ -35,14 +43,64 @@ price = {'ACME': 45.23,
          'FB': 10.75}
 max_price = max(zip(price.values(),price.keys()))  # zip()结合max()求最大最小值
 sorted_price = sorted(zip(price.values(),price.keys())) # zip()结合sorted()排序
-# zip()创建了一个迭代器，交换key和value, 并且只交换一次，求完max之后就恢复
-# max(),sorted()都可以对字典操作，但只能对key操作不能对value操作，所以才要zip()交换
+# zip()创建了一个迭代器，并把数值放前面，便于使用各种函数比如sorted()
 # 该方法优于 max(price.values())，因为能同时返回key和value
 # 如果遇到2个最大/小值情况，返回的是两个中键更大/小的那个
 
 
 '''
-Q: ？
+Q: 有一组数据，如何获得最大的或最小的N个元素？
 '''
+num = [1,8,2,23,7,-4,18,23,42,37,2]
+max_3 = sorted(num)[:3]   # 通过先排序再切片的方式获得最大/最小的N个元素
+
+
+'''
+Q: 想创建一个字典，并且希望字典内保持输入时的顺序，而不是默认的key字母顺序？
+'''
+from collections import OrderedDict
+d = OrderedDict()   # 通过OrderedDict类创建一个带顺序的字典，按添加顺序保存
+d['foo'] = 2
+d['bar'] = 1
+d['spam'] = 3
+for key in d:
+    print(key, d[key])
+
+
+'''
+Q: 有一组序列，希望找到序列中出现次数的元素，甚至每个元素的出现次数，怎么做？
+'''
+lst1 = [1,3,2,4,5,7,8,9,1,2,6,9,0,
+       8,3,1,8,5,2,8,3,0,3,6,8,9,
+       0,9,7,4,7,3,1,7,5,7,0,4,0]
+lst2 = [0,0,1,0]
+from collections import Counter
+num_count = Counter(lst1)   # 使用Counter()函数，可以统计每个元素出现次数，返回list或dict
+num_count.update(lst2)      # counter.update()可以增加计数变量的update方法
+num_count[0]                # 类似字典或list的调用方法
+
+
+'''
+Q: 有一组序列，希望对某个键进行排序，用sorted（）怎么定义？
+'''
+rows = [{'fname':'David', 'lname':'Jones', 'uid': 1003},
+        {'fname':'Brian', 'lname':'Beazley', 'uid': 1002}]
+from operator import itemgetter
+sort_fname = sorted(rows, key = itemgetter('fname')) 
+# 用itemgetter()函数获得item的‘fname’字段
+
+data = [('b', 2), ('a',3), ('c',1)]
+sort_data = sorted(data, key = itemgetter(0))  
+# 用itemgetter()获得每个item的第0元素或第1元素
+
+
+'''
+Q: 有一组序列，希望筛选出需要的元素，怎么筛选？
+'''
+mylist = [1,4,-5,10,-7,2,3,-1]
+pos = [n for n in mylist if n > 0]     # 用列表推导式做筛选
+pos_or_zero = [n if n>0 else 0 for n in mylist] # 用列表推导式筛选
+# 列表推导式 [n for xx if xxx] 先for循环，再条件判断
+# 列表推导式 [n if xx for xxx] 先条件判断，再进行for循环
 
 
