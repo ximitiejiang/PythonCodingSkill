3d48a171356ab9c61bab9a0e4e1876601cd2e812
ximitiejiang <ximitiejiang@163.com>
2019-02-16 23:16:05 +0800

    update

--- 9_temp_test.py
+++ 9_temp_test.py
@@ -83,7 +83,7 @@ def _meshgrid(x, y, row_major=True):
     else:
         return yy, xx
     
-def grid_anchors(featmap_size, stride, base_anchors, device='cuda'):
+def grid_anchors(featmap_size, stride, base_anchors, device='cpu'):
     base_anchors = base_anchors.to(device)
 #
     feat_h, feat_w = featmap_size
@@ -109,11 +109,14 @@ def grid_anchors_mine(featmap_size, stride, base_anchors):
         featmap_size(list(float))
         stride(float): 代表该特征图相对于原图的下采样比例，也就代表每个网格的感受野是多少尺寸的原图网格，比如1个就相当与stride x stride大小的一片原图
         device
-    1. 分割
-    2. 
+    1. 先计算该特征图对应原图像素大小 = 特征图大小 x 下采样比例
+    2. 然后生成网格坐标xx, yy并展平：先得到x坐标，再meshgrid思想得到网格xx坐标，再展平
+       其中x坐标就是按照采样比例，每隔1个stride取一个坐标
+    3. 然后堆叠出[xx,yy,xx,yy]分别叠加到anchor原始坐标[xmin,ymin,xmax,ymax]上去(最难理解，广播原则)
+    4. 最终得到特征图上每个网格点上都安放的n个base_anchors
     """
     feat_h, feat_w = featmap_size
-    shift_x = torch.arange(0, feat_w) * stride  # 先放大到原图大小 (256,)
+    shift_x = torch.arange(0, feat_w) * stride  # 先放大到原图大小 (256)
     shift_y = torch.arange(0, feat_h) * stride  #                 (152)
     shift_xx = shift_x[None,:].repeat((len(shift_y), 1))   # (152,256)
     shift_yy = shift_y[:, None].repeat((1, len(shift_x)))  # (152,256)
@@ -121,25 +124,22 @@ def grid_anchors_mine(featmap_size, stride, base_anchors):
     shift_xx = shift_xx.flatten()   # (38912,) 代表了原始图的每个网格点x坐标，用于给x坐标平移
     shift_yy = shift_yy.flatten()   # (38912,) 代表了原始图的每个网格点y坐标，用于给y坐标平移
     
-    shifts = torch.stack([shift_xx, shift_yy, shift_xx, shift_yy], dim=-1) # 堆跌给[xmin,ymin,xmax,ymax], (38912,4)
+    shifts = torch.stack([shift_xx, shift_yy, shift_xx, shift_yy], dim=-1) # 堆叠成4行给4个坐标[xmin,ymin,xmax,ymax], (38912,4)
     shifts = shifts.type_as(base_anchors)   # 从int64转换成torch认可的float32
-    # first feat_w elements correspond to the first row of shifts
-    # add A anchors (1, A, 4) to K shifts (K, 1, 4) to get
-    # shifted anchors (K, A, 4), reshape to (K*A, 4)
+    
+    # anchor坐标(9,4)需要基于网格坐标(38912,4)进行平移：平移后应该是每个网格点有9个anchor
+    # 也就是38912个(9,4)，也就等效于anchor(9,4)与每一个网格坐标(1,4)进行相加
+    # 需要想到把(38912,4)提取出(1,4)的方式是升维到(38912,1,4)与(9,4)相加
+    all_anchors = base_anchors + shifts[:,None,:]   # 利用广播法则(9,4)+(38912,1,4)->(39812,9,4)
+    all_anchors = all_anchors.view(-1,4)            # 部分展平到(n,4)得到每个anchors的实际坐标(图像左上角为(0,0)原点)                      
 
-    all_anchors = base_anchors[None, :, :] + shifts[:, None, :]   # (1,9,4) + (38912,1,4)
-    all_anchors = all_anchors.view(-1, 4)                         # (9,38912,4) -> (9x38912, 4)
-    # first A rows correspond to A anchors of (0, 0) in feature map,
-    # then (0, 1), (0, 2), ...
     return all_anchors
     
     
-
-
 import torch    
 anchor_strides = [4., 8., 16., 32., 64.]
 anchor_base_sizes = anchor_strides      # 基础尺寸
-anchor_scales = [8., 16., 32.]             # 缩放比例
+anchor_scales = [8., 16., 32.]          # 缩放比例
 anchor_ratios = [0.5, 1.0, 2.0]         # w/h比例
 
 num_anchors = len(anchor_scales) * len(anchor_ratios)
