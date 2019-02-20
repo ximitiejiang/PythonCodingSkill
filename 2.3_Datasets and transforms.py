96777d12f6e59f7acaf554b1605fa220d5f25c30
ximitiejiang <ximitiejiang@163.com>
2019-02-21 00:09:08 +0800

    update

---
+++ 2.3_Datasets and transforms.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 20 23:26:11 2019
+
+@author: suliang
+"""
+
+# %%
+"""如何读取VOC dataset？
+"""
+
+
+
+
+# %%
+"""如何读取COCO dataset？
+"""
