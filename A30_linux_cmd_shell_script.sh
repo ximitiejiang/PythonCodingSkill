012685e38833dd366b7a443d9e965b9d65be2d7a
ximitiejiang <ximitiejiang@163.com>
2019-04-05 13:25:24 +0800

    update

---
+++ A30_linux_cmd_shell_script.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 27 12:25:49 2019
+
+@author: ubuntu
+"""
+# %%
+"""Q. 安装软件用什么？
+1. sudo dpkg -i document_name
+    >其中dpkg代表：debian linux dc，是专门为debian专门的安装管理系统，所哟debian
+     的linux发行版都使用这个管理系统
+    >指令sudo dpkg -i|--install 代表安装
+    >指令sudo dpkg -r|--remove 代表卸载
+    >指令
+"""
+
+
+# %%
+"""Q. 文件夹切换命令
+
+"""
+cd document1  # 进入目录
+cd ..  # 上一层目录
+
+
+# %%
+"""Q.显示文件夹下的文件"""
+ls
+
+
+# %%
+"""Q.如何删除文件"""
+rm test.txt   # 删除文件
+rm -rf path
+
+# %%
+"""Q.显示文件夹路径"""
+pwd
+
+
+# %%
+"""Q.创建文件夹"""
+mkdir /data/path
+
+
+# %%
+"""Q.结束进程"""
+ps au  # 查看进程
+kill PID_n  # 结束具体进程的号
+
+
+# %%
+"""Q.基础shell脚本
+"""
+cd /tmp
+for i in /tmp/*:
+    
+    
+    
+    
+    
+    
