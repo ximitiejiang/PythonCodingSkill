5103f0f66b7e268ebc19312b9df69db89cb88ccb
ximitiejiang <ximitiejiang@163.com>
2019-07-12 18:41:54 +0800

    update

--- test.sh
+++ test.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 
-ex(){
-echo "hello woooooorld!"
-echo "try to explain"
-有意识
-}
-
-demoFun(){
-    echo "this is my first func!"
-}
-
-demoFun
-
-
-:<<!
-这是注释
-这是说明
-!
+a=100
+echo ${a}
