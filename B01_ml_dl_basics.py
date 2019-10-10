453b2c0f1b21c3d92686ded5fcac71caf58cf2a2
ximitiejiang <ximitiejiang@163.com>
2019-10-10 18:39:37 +0800

    update

--- B01_ml_dl_basics.py
+++ B01_ml_dl_basics.py
@@ -892,11 +892,18 @@ loss_out = torch.sum(torch.mul(output, y_one_hot), dim=1).mean()
    这里他采用的是标签概率*分类概率，标签概率是1或者0，而在pytorch是用标签映射直接得到-log概率后做规约，底层逻辑
    都是一样的，都是采用真实概率Pa与模型概率Pb的乘积的累加和作为损失函数，本质一样都是通过交叉熵度量2个不同分布的距离(伪距离)
    而二分类问题通常用sigmoid函数概率化，多分类问题用softmax函数概率化"""
+   
+# 多分类交叉熵用法1：   
+imgs = torch.randn(4, 21, 30, 30)  # 图像
+labels = torch.randn()               # 像素标签((0-20)
+loss = F.cross_entropy(imgs, labels, reduction='none')
+   
+# 多分类交叉熵用法2：
 imgs = torch.tensor([[-0.5883,  1.4083, -1.9200,  0.4291, -0.0574],
                      [ 1.5962,  2.2646, -0.2490,  0.1534, -0.5345],
                      [-0.2562, -0.4440, -0.1629,  0.8097,  0.6865]], requires_grad=True)
 labels = torch.tensor([2, 0, 4], dtype=torch.int64)  # pytorch的交叉熵函数要求label格式为int64/也就是LongTensor
-loss1 = F.cross_entropy(imgs, labels)   # loss = 2.1105 (默认已经缩减)
+loss1 = F.cross_entropy(imgs, labels, reduction='none')   # loss = 2.1105 (默认已经缩减)
 # 纯手动实现多分类交叉熵：[exp+概率化+log]+[(-1)+概率相乘]，前半截在logsoftmax完成，后半截在nll_loss完成，也可以全部在cross_entropy中一起完成
 # 对比二值交叉熵的过程： [sigmoid]+[log+(-1)+概率相乘]，前半截在sigmoid完成，后半截在binary_cross_entropy完成，也可以全部在binary_cross_entropy_with_logits一起完成
 n_img, n_class = imgs.shape
